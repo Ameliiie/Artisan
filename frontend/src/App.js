efe59cc33d7dd3271601c1c0b836b10b54402585
AmelieBarbier <amliedu28@gmail.com>
2026-07-04 22:36:06 +0200

    feat: création du Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,21 @@ import Home from "./pages/Home";
 import ArtisanList from "./pages/ArtisanList";
 import ArtisanDetails from "./pages/ArtisanDetails";
 import NotFound from "./pages/NotFound";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/artisans" element={<ArtisanList />} />
         <Route path="/artisan/:id" element={<ArtisanDetails />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+       <Footer />
     </BrowserRouter>
   );
 }
